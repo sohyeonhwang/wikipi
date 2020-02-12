9150a5b6c7af0dd853d91fb8ad3076e749dd0b00
sohyeonhwang <sohyeonhwang2024@u.northwestern.edu>
2020-02-12 12:08:03 -0600

    sparky files

--- sparky/output_crunching.py
+++ sparky/output_crunching.py
@@ -1,7 +1,7 @@
 import sys
 # add pyspark to your python path e.g.
-#sys.path.append("/home/nathante/sparkstuff/spark/python/pyspark")
-#sys.path.append("/home/nathante/sparkstuff/spark/python/")
+sys.path.append("/home/sohw/sparkstuff/spark/python/pyspark")
+sys.path.append("/home/sohw/sparkstuff/spark/python/")
 from pyspark import SparkConf
 from pyspark.sql import SparkSession, SQLContext
 from pyspark.sql import Window
@@ -9,8 +9,9 @@ import pyspark.sql.functions as f
 from pyspark.sql import types
 import argparse
 import glob
-from os import mkdir
-from os import path
+import csv
+from pathlib import Path
+import os
 from wikiq_util import PERSISTENCE_RADIUS
 #read a table
 
@@ -21,7 +22,7 @@ def parse_args():
     parser.add_argument('-o', '--output-dir', help='Output directory', default='./output', type=str)
 #    parser.add_argument('--wiki', help="Wiki name. If not provided, we will guess based on the filename.", type=str)
     parser.add_argument('--urlencode', help="whether we need to decode urls",action="store_true")
-    parser.add_argument('--output-format', help = "[csv, parquet] format to output",type=str)
+    parser.add_argument('-f','--output-format', help = "[csv, parquet] format to output",type=str)
     parser.add_argument('--num-partitions', help = "number of partitions to output",type=int, default=1)
     parser.add_argument('--schema-opt', help = 'Options for the input schema.', choices = ["basic","persistence","collapse","persistence+collapse"])
 #    parser.add_argument('--nodes', help = "how many hyak nodes to use", default=0, type=int)
@@ -37,5 +38,53 @@ if __name__ == "__main__":
     files = glob.glob(args.input_file)
     files = [path.abspath(p) for p in files]
 
+    read_collapse = args.schema_opt in ["collapse", "persistence+collapse"]
+    read_persistence = args.schema_opt in ["persistence", "persistence+collapse"]
+
+    if read_collapse is True:
+        struct = struct.add("collapsed_revs", types.IntegerType(), True)
+
+    struct = struct.add("date_time",types.TimestampType(), True)
+    struct = struct.add("deleted",types.BooleanType(), True)
+    struct = struct.add("editor",types.StringType(),True)
+    struct = struct.add("editor_id",types.LongType(), True)
+    struct = struct.add("minor", types.BooleanType(), True)
+    struct = struct.add("namespace", types.LongType(), True)
+    struct = struct.add("revert", types.BooleanType(), True)
+    struct = struct.add("reverteds", types.StringType(), True)
+    struct = struct.add("revid", types.LongType(), True)
+    struct = struct.add("sha1", types.StringType(), True)
+    struct = struct.add("text_chars", types.LongType(), True)
+    struct = struct.add("title",types.StringType(), True)
+
+    if read_persistence is True:
+        struct = struct.add("token_revs", types.IntegerType(),True)
+        struct = struct.add("tokens_added", types.IntegerType(),True)
+        struct = struct.add("tokens_removed", types.IntegerType(),True)
+        struct = struct.add("tokens_window", types.IntegerType(),True)
+
+    # we want to also add structures for the columns 
+    wd = Path(os.getcwd())
+    columns_file_path = wd / 'eswiki_columns'
+
+    columnsToMerge = []
+    #wp_columns_struct = wp_columns_struct.add("revid", types.LongType(), True)
+
+    with open(columns_file_path.as_posix()) as tsv:
+        rows = csv.reader(tsv, delimiter='\t')
+        for row in rows:
+            columnsToMerge.append(row[0])
+            struct = struct.add(row[0],types.StringType(), True)
+            #wp_columns_struct = wp_columns_struct.add(row[0],types.StringType(), True)
+
+    reader = spark.read
+
+    df = reader.csv(files,
+                    sep="\t",
+                    inferSchema=False,
+                    header=True,
+                    mode="PERMISSIVE",
+                    schema=struct)
+    df = df.repartition(args.num_partitions)
 
     
---
+++ sparky/snippet_tests.py
@@ -0,0 +1,22 @@
+import sys
+# add pyspark to your python path e.g.
+sys.path.append("/home/sohw/sparkstuff/spark/python/pyspark")
+sys.path.append("/home/sohw/sparkstuff/spark/python/")
+
+import glob
+from pathlib import Path
+import os
+import csv
+
+# iterating through the column names
+wd = Path(os.getcwd())
+columns_file_path = wd / 'frwiki_columns'
+
+n = 0
+with open(columns_file_path.as_posix()) as tsv:
+    reader = csv.reader(tsv, delimiter='\t')
+    for row in reader:
+        n += 1
+        print(n)
+        print(row[0])
+        print(type(row[0]))
