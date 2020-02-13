1a07f108acb6642ce93231adb4205f75f4b941a2
sohyeonhwang <sohyeonhwang@u.northwestern.edu>
2020-02-13 00:51:41 -0800

    snippet_test

--- sparky/snippet_tests.py
+++ sparky/snippet_tests.py
@@ -22,9 +22,9 @@ samples = ["eswiki_sample.tsv"]
 for s in samples:
 	s_path = samples_folder / s
 	print(s_path.as_posix())
-	with open(s_path.as_posix()) as tsv:
-		reader = csv.reader(tsv, delimiter='\t')
-		for row in reader:
+	#with open(s_path.as_posix()) as tsv:
+	#	reader = csv.reader(tsv, delimiter='\t')
+	#	for row in reader:
 		
 
 '''
