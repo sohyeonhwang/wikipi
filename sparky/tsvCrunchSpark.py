3a633e99fe7950bed86c911d485ad0defbb39aab
sohyeonhwang <sohyeonhwang2024@u.northwestern.edu>
2020-05-04 10:28:01 -0500

    bug fix test

--- sparky/tsvCrunchSpark.py
+++ sparky/tsvCrunchSpark.py
@@ -19,38 +19,6 @@ def parse_args():
     args = parser.parse_args()
     return(args)
 
-def coreColumns(onlyRegexCols):
-    #CORE COLUMNS
-    #ENGLISH:
-    #SPANISH:	53_WIKIPEDIA:PUNTO_DE_VISTA_NEUTRAL, 69_WIKIPEDIA:WIKIPEDIA_NO_ES_UNA_FUENTE_PRIMARIA, 64_WIKIPEDIA:VERIFICABILIDAD
-    #FRENCH: 	26_WIKIPÉDIA:NEUTRALITÉ_DE_POINT_DE_VUE, 19_WIKIPÉDIA:TRAVAUX_INÉDITS, 21_WIKIPÉDIA:VÉRIFIABILITÉ
-    #GERMAN:
-    #JAPANESE:	14_WIKIPEDIA:中立的な観点,16_WIKIPEDIA:独自研究は載せない, 15_WIKIPEDIA:検証可能性
-    
-    if "53_WIKIPEDIA:PUNTO_DE_VISTA_NEUTRAL" in onlyRegexCols:
-        coreDFColumn = [c for c in onlyRegexCols if (c[:2]==str(53) or c[:2]==str(69) or c[:2]==str(64))]
-    elif "26_WIKIPÉDIA:NEUTRALITÉ_DE_POINT_DE_VUE" in onlyRegexCols:
-        coreDFColumn = [c for c in onlyRegexCols if (c[:2]==str(26) or c[:2]==str(19) or c[:2]==str(21))]
-    else:
-        coreDFColumn = [c for c in onlyRegexCols if (c[:2]==str(14) or c[:2]==str(15) or c[:2]==str(16))]
-
-    return coreDFColumn
-
-def cumulMonthly(regex_df):
-    onlyRegexCols = [c for c in regex_df.columns if c[0].isdigit()]
-    coreDFColumn = coreColumns(onlyRegexCols)
-
-    monthly_regex_df = regex_df.select(regex_df.revid, f.concat_ws('_',f.year(regex_df.date_time),f.month(regex_df.date_time)).alias('year_month'),f.concat_ws(', ',*coreDFColumn).alias('core_regex'))
-    monthly_regex_df = monthly_regex_df.na.replace('',None)
-    monthly_regex_df = monthly_regex_df.select(*monthly_regex_df,f.when(monthly_regex_df.core_regex.isNotNull(),1).otherwise(0).alias('core_policy_invoked'))
-
-    monthly_core_count_df = monthly_regex_df.groupBy('year_month').sum('core_policy_invoked')
-    monthly_revn_count_df = monthly_regex_df.groupBy('year_month').count()
-
-    monthly_joined_df = monthly_revn_count_df.join(monthly_core_count_df, on=['year_month'],how='left')
-
-    return monthly_joined_df
-
 def df_diff(input_df):
     # input_df should be regex_df in df_structurize
     regex_diff_df = input_df.orderBy("articleid")
@@ -90,7 +58,7 @@ def df_structurize(input_df, struct):
 
     return meta_df, regex_df
 
-def sparkit(wikiqtsv):
+def df_regex_make(wikiqtsv):
     # make wikiq tsv into a dataframe
     tsv2df = reader.csv(wikiqtsv,
                         sep="\t",
@@ -118,14 +86,39 @@ def sparkit(wikiqtsv):
     # structure the df to get the def with columns of metadata and regexes
     meta_df, regex_df = df_structurize(tsv2df,struct)
 
-    print(type(regex_df))
-    regex_df.show()
+    return regex_df
 
-    cumul_month = cumulMonthly(regex_df)
-    #cumul_month.orderBy(cumul_month.year_month).show()
+def findCoreColumns(onlyRegexCols):
+    #CORE COLUMNS
+    #ENGLISH:
+    #SPANISH:	53_WIKIPEDIA:PUNTO_DE_VISTA_NEUTRAL, 69_WIKIPEDIA:WIKIPEDIA_NO_ES_UNA_FUENTE_PRIMARIA, 64_WIKIPEDIA:VERIFICABILIDAD
+    #FRENCH: 	26_WIKIPÉDIA:NEUTRALITÉ_DE_POINT_DE_VUE, 19_WIKIPÉDIA:TRAVAUX_INÉDITS, 21_WIKIPÉDIA:VÉRIFIABILITÉ
+    #GERMAN:
+    #JAPANESE:	14_WIKIPEDIA:中立的な観点,16_WIKIPEDIA:独自研究は載せない, 15_WIKIPEDIA:検証可能性
+    
+    if "53_WIKIPEDIA:PUNTO_DE_VISTA_NEUTRAL" in onlyRegexCols:
+        coreDFColumn = [c for c in onlyRegexCols if (c[:2]==str(53) or c[:2]==str(69) or c[:2]==str(64))]
+    elif "26_WIKIPÉDIA:NEUTRALITÉ_DE_POINT_DE_VUE" in onlyRegexCols:
+        coreDFColumn = [c for c in onlyRegexCols if (c[:2]==str(26) or c[:2]==str(19) or c[:2]==str(21))]
+    else:
+        coreDFColumn = [c for c in onlyRegexCols if (c[:2]==str(14) or c[:2]==str(15) or c[:2]==str(16))]
 
-    return cumul_month
+    return coreDFColumn
 
+def df_cumulMonthly_make(regex_df):
+    onlyRegexCols = [c for c in regex_df.columns if c[0].isdigit()]
+    coreDFColumn = findCoreColumns(onlyRegexCols)
+
+    monthly_regex_df = regex_df.select(regex_df.revid, f.concat_ws('_',f.year(regex_df.date_time),f.month(regex_df.date_time)).alias('year_month'),f.concat_ws(', ',*coreDFColumn).alias('core_regex'))
+    monthly_regex_df = monthly_regex_df.na.replace('',None)
+    monthly_regex_df = monthly_regex_df.select(*monthly_regex_df,f.when(monthly_regex_df.core_regex.isNotNull(),1).otherwise(0).alias('core_policy_invoked'))
+
+    monthly_core_count_df = monthly_regex_df.groupBy('year_month').sum('core_policy_invoked')
+    monthly_revn_count_df = monthly_regex_df.groupBy('year_month').count()
+
+    monthly_joined_df = monthly_revn_count_df.join(monthly_core_count_df, on=['year_month'],how='left')
+
+    return monthly_joined_df
 
 if __name__ == "__main__":
     args = parse_args()
@@ -143,15 +136,17 @@ if __name__ == "__main__":
 
     for f in sample:
         print(f)
-        cumulMonthly = sparkit(f)
-        print(type(cumulMonthly))
+        regex_df = df_regex_make(f)
+        # make it monthly
+        print(type(regex_df))
+
 
         print("\n\n======================================================================================================\n\n")
 
         #monthly_dfs.append(cumulMonthly)
-        cumulMonthlyPandas = cumulMonthly.toPandas()
-        print(type(cumulMonthlyPandas))
-        cumulMonthlyPandas.head()
+        #cumulMonthlyPandas = cumulMonthly.toPandas()
+        #print(type(cumulMonthlyPandas))
+        #cumulMonthlyPandas.head()
 
 
 
