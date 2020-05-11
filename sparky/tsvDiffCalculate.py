79e5b89da15b4cab74ff10e457628e7cbdb0585c
sohyeonhwang <sohyeonhwang2024@u.northwestern.edu>
2020-05-11 13:13:57 -0500

    tsvDiffCalculate imap, not map

--- sparky/tsvDiffCalculate.py
+++ sparky/tsvDiffCalculate.py
@@ -231,7 +231,7 @@ def parallelize_dataframe(df, func, n_cores=30):
     print("Using {} cores, applying {} to {}".format(n_cores, func, df))
     df_split = np.array_split(df, n_cores)
     pool = Pool(n_cores)
-    df = pd.concat(pool.map(func, df_split))
+    df = pd.concat(pool.imap(func, df_split))
     pool.close()
     pool.join()
     return df
