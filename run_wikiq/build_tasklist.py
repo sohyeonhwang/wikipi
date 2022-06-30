e6916642ad75b08a78ebe6a7ed6479258bc9c025
S. Hwang <sohw@mox2.hyak.local>
2022-06-29 22:11:17 -0700

    tasklist building code

---
+++ run_wikiq/build_tasklist.py
@@ -0,0 +1,55 @@
+import requests
+import re, os, csv, glob
+import pandas as pd
+import time
+import json
+from collections import Counter
+from pathlib import Path
+
+langs = ['de','en','es','fr','ja']
+
+project_dir = Path.cwd().parent.parent
+print(project_dir)
+raw_data = project_dir / 'raw_data'
+
+regexes = pd.read_csv('rule_regex_generated.tsv',sep='\t',header=0)
+
+all_calls = []
+
+for lang in langs:
+    calls = []
+
+    print(lang)
+    lang_data_dir = raw_data / '{}wiki'.format(lang)
+    dumps = glob.glob('{}/*'.format(lang_data_dir.as_posix()))
+
+    print(lang_data_dir, len(dumps))
+
+    # construct the giant regex -RP and -RPl pairs
+    regex_statement = ''
+    lang_regexes = regexes.loc[regexes.lang==lang]
+
+    pattern_pairs = list(zip(lang_regexes.label,lang_regexes.regex))
+    for p in pattern_pairs:
+        pattern = '-RP "{}" -RPl "{}"'.format(p[1],p[0]) 
+        regex_statement = '{} {}'.format(regex_statement,pattern)
+    print(regex_statement)
+
+    # for each dump
+    for dump in dumps:
+        call = './run_wikiq.sh {} {}'.format(
+            dump,
+            regex_statement
+        )
+    
+        calls.append(call)
+        all_calls.append(call)
+    
+    # make the lang-specific task list
+    with open("tasklist_{}.sh".format(lang),"w") as f:
+        for c in calls:
+            f.write("{}\n".format(c))
+
+    with open('task_list.sh'.format(lang),'w') as f:
+        for c in all_calls:
+            f.write('{}\n'.format(c))
